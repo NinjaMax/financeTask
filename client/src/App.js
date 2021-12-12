6259bc9295f06ee6a59bbb76e6d8b0287fcd6fab
Max <unitigrand@gmail.com>
2021-12-12 17:37:12 +0200

    add App index

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import AppRouter from './components/AppRouter';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import {observer} from "mobx-react-lite";
+
+
+const App = observer(() => {
+  
+ 
+  return (
+    <BrowserRouter >
+      <NavBar/>
+        <AppRouter />
+      <Footer/>
+    </BrowserRouter>
+  );
+});
+
+export default App;
